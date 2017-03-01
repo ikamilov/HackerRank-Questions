0a4b8b7f0af760e1c977c98c1cf891e90f92a00b
ikamilov <kamilov6@gmail.com>
2017-03-01 14:17:31 -0600

    java

---
+++ Java/maximumperimeter.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */    Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int[] a = new int[n];
+        for(int i = 0; i < n; i++) {
+            a[i] = scanner.nextInt();
+        }
+        scanner.close();
+        Arrays.sort(a);
+        int x, y, z;
+        for(x=n-3, y=n-2, z=n-1; a[x]+a[y]<=a[z]; x--, y--, z--){
+            if(x==0) {
+                System.out.print(-1);
+                return;
+            }
+        }
+        System.out.printf("%d %d %d", a[x], a[y], a[z]);
+    }
+}
