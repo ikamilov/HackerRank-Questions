9c952c39f6199b38537494371d0da15615891f23
ikamilov <kamilov6@gmail.com>
2017-07-24 23:27:16 -0500

    all

---
+++ Algorithms/Python/Codechef/sumsintriangle.py
@@ -0,0 +1,22 @@
+for q in range(int(input())):
+	adding = []
+	a = int(input())
+	for k in range(a):
+		x = input().split()
+		adding.append(x)
+		
+	print(int(adding[0][1]) + int(adding[2][1]))
+"""
+if k < 3:
+			if k == a:
+				adding.append(int(x))
+				adding.remove(int(x[0]))
+				adding.remove(int(x[1]))
+			elif k == a - 1:
+				adding.append(int(x))
+				adding.remove(int(x[0]))
+			else:
+				adding.append(int(x))
+		else: 
+			
+"""
