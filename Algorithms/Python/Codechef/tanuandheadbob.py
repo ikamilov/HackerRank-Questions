8041c5b9726b626269fdbbc8661cad1d7e3bb567
ikamilov <kamilov6@gmail.com>
2017-08-04 19:51:33 -0500

    added

--- Algorithms/Python/Codechef/tanuandheadbob.py
+++ Algorithms/Python/Codechef/tanuandheadbob.py
@@ -1,4 +1,13 @@
 for i in range(int(input())):
 	n = int(input())
-	s = list(input())
-	print(s[1])
+	s = input()
+	for k in s:
+		if k == 'I':
+			ans = "INDIAN"
+			break
+		elif k == 'Y':
+			ans = "NOT INDIAN"
+			break
+		else:
+			ans = "NOT SURE"
+	print(ans)
