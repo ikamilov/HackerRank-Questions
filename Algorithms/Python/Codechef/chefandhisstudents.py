c82da6cef4a0663a0428a04a72968b488e586176
ikamilov <kamilov6@gmail.com>
2017-10-10 23:15:28 -0500

    added

--- Algorithms/Python/Codechef/chefandhisstudents.py
+++ Algorithms/Python/Codechef/chefandhisstudents.py
@@ -1,12 +1,2 @@
 for test in range(int(input())):
-	s = list(input().strip())
-	count = 0
-	for i, j in enumerate(s[:1]):
-		if j != s[i + 1]:
-			if (j == "<" and s[i + 1] == ">") or (s[i+1] == "<" and j == ">"):
-				count += 1
-		else:
-			count += 0
-		
-	print(count)
-
+	print (input().count("<>"))
